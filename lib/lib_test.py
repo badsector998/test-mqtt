a85287e885946cc4b60c33eb2d586a142aa20280
DadangRokes <harddiskkosong@gmail.com>
2022-09-03 11:31:41 +0700

    Update lib_test.py

--- lib/lib_test.py
+++ lib/lib_test.py
@@ -81,9 +81,8 @@ class MqttTestClass(unittest.TestCase):
         data = dbInstance.executeQuery("query.txt")
         mqttIdTest, PayloadContent = repo.parseData(data)
         Payload = repo.createPayload(mqttIdTest, PayloadContent)
-        info = mqttClient.sendPayload(Payload)
         self.assertIsInstance(
-                        info,
+                        mqttClient.sendPayload(Payload),
                         mqtt.mqtt.MQTTMessageInfo
                     )
 
